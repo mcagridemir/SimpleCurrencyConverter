602d108a52806eb82e2cf83d14d9e99960e6819a
mcagridemir <cagriidemirr@gmail.com>
2020-04-19 23:35:52 +0300

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Çağrı Demir on 19.04.2020.
+//  Copyright © 2020 Cagri Demir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
